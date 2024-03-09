1d3067ad00f091f79010c170f3e74366fe51ce49
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-09 19:58:47 +0000

    style header component and add external icons

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const Header = () => (
   <div className="heroSection">
-    <div className="logo">LOGO</div>
-    <div className="search">
-      <input type="search" name="Searchbar" id="searchbar" />
+    <div className="logo">
+      <i class="ri-restaurant-fill"></i>FOODIE
     </div>
-    <div className="userIcon">userIcon</div>
+    <div className="search">
+      <i class="ri-search-2-line"></i>
+      <input
+        type="search"
+        name="Searchbar"
+        id="searchbar"
+        placeholder="Search here..."
+      />
+    </div>
+    <div className="userIcon">
+      <i class="ri-shield-user-line"></i>
+    </div>
   </div>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
