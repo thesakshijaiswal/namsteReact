a6862620baa847d69cda91ec12d9680ec0d8cda0
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-09 14:26:49 +0000

    Asgn4: Composing component

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const heading = React.createElement(
-  "h1",
-  { id: "title", key: "h1" },
-  "Hello react",
-);
-const Heading2 = () => (
-  <ul>
-    <li key="l1">Home</li>
-    <li key="l2">About us </li>
-    <li key="l3">Blogs </li>
-  </ul>
-);
-//This how jsx looks
-const heading3 = <h1>Now im utilising jsx</h1>;
-
-// Components in React
-
-//- Functional component (New)
-const HeaderComponent = () => {
+const Title = () => <h1>Namaste react assignment 4 question 1</h1>;
+const Header = () => {
   return (
-    <div>
-      <Heading2 />
-      {heading}
-      {
-        //any js code can go inside of this
-        console.log(20 + 30)
-      }
-      <h3>This is functional component</h3>
-      <p>description for functional component</p>
+    <div className="title">
+      <Title />
+      <h1>Hello React</h1>
+      <h2>welcome to the world of react</h2>
+      <h3>Assignment 4</h3>
     </div>
   );
 };
-// -Class component (Old)
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// render a jsx component using below code line
-root.render(<HeaderComponent />);
+root.render(<Header></Header>);
