25a6b18df47a1ddad06ad9afd42bf2c1baaa88d8
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-10 10:57:25 +0000

    Step toward building a food ordering app

--- App.js
+++ App.js
@@ -3,19 +3,7 @@ import ReactDOM from "react-dom/client";
 const Header = () => (
   <div className="heroSection">
     <div className="logo">
-      <i class="ri-restaurant-fill"></i>FOODIE
-    </div>
-    <div className="search">
-      <i className="ri-search-2-line"></i>
-      <input
-        type="search"
-        name="Searchbar"
-        id="searchbar"
-        placeholder="Search here..."
-      />
-    </div>
-    <div className="userIcon">
-      <i class="ri-shield-user-line"></i>
+      <i class="ri-restaurant-fill"></i>FoodFunk
     </div>
   </div>
 );
