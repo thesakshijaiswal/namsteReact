4fa3bc1c5f68d7ac2a4ca2936a9f91c3311171de
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-11 11:13:23 +0000

    inline styling in react

--- App.js
+++ App.js
@@ -2,24 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 const Header = () => (
   <div className="navbar">
-    <div className="logo">
+    <div className="logo" style={{ color: "brown" }}>
       FoodFunk
       <i className="ri-bowl-fill"></i>
     </div>
     <ul>
       <li>
-        Home<i class="ri-home-smile-fill"></i>
+        Home<i className="ri-home-smile-fill"></i>
       </li>
       <li>
-        About<i class="ri-hotel-fill"></i>
+        About<i className="ri-hotel-fill"></i>
       </li>
       <li>
-        Offers<i class="ri-money-rupee-circle-fill"></i>
+        Offers<i className="ri-money-rupee-circle-fill"></i>
       </li>
       <li>
-        Cart<i class="ri-shopping-cart-2-fill"></i>
+        Cart<i className="ri-shopping-cart-2-fill"></i>
       </li>
-      <i class="ri-menu-3-line"></i>
+      <i className="ri-menu-3-line"></i>
     </ul>
   </div>
 );
