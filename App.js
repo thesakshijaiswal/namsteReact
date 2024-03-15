c71fb79ef59c972ba83ff576dbaed3749c548c05
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-15 10:16:11 +0000

    Add multiple restaurant cards

--- App.js
+++ App.js
@@ -24,28 +24,52 @@ const Header = () => (
   </div>
 );
 
-const cardContent = {
-  image:
-    "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  name: "Burger King",
-  cuisines: ["American", "Burger"],
-  rating: "4.5",
-};
+const cardContent = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1586190848861-99aa4a171e90?q=80&w=2680&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Burger King",
+    cuisines: ["American", "Burger"],
+    rating: "4.5",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1668236543090-82eba5ee5976?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "kitchen king",
+    cuisines: ["South indian", "uttapam"],
+    rating: "4",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=2762&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "KFC",
+    cuisines: ["noddles", "Pizza", "Burger"],
+    rating: "4.6",
+  },
+];
 const RestaurantCard = ({ image, name, cuisines, rating }) => {
   return (
     <div className="foodCard">
-      <img src={image} alt="American Burger" />
-      <h2>{name}</h2>
-      <p>{cuisines.join(", ")}</p>
-      <h4>
-        Ratings: {rating}
-        <i className="ri-star-fill"></i>
-      </h4>
+      <div className="cardInfo">
+        <img src={image} alt="American Burger" />
+        <h2>{name}</h2>
+        <p>{cuisines.join(", ")}</p>
+        <h4>
+          Ratings: {rating}
+          <i className="ri-star-fill"></i>
+        </h4>
+      </div>
     </div>
   );
 };
 const Body = () => {
-  return <RestaurantCard {...cardContent} />;
+  return (
+    <div>
+      <RestaurantCard {...cardContent[0]} />
+      <RestaurantCard {...cardContent[1]} />
+      <RestaurantCard {...cardContent[2]} />
+    </div>
+  );
 };
 const AppLayout = () => {
   return (
