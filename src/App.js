e5977ad0a7cb1380bac95825ec4f5dfb5cf2c4ae
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-16 12:21:57 +0000

    export navbar from component folder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const Header = () => (
-  <div className="navbar">
-    <div className="logo">
-      FoodFunk
-      <i className="ri-bowl-fill"></i>
-    </div>
-    <ul>
-      <li>
-        Home<i className="ri-home-smile-fill"></i>
-      </li>
-      <li>
-        About<i className="ri-hotel-fill"></i>
-      </li>
-      <li>
-        Offers<i className="ri-money-rupee-circle-fill"></i>
-      </li>
-      <li>
-        Cart<i className="ri-shopping-cart-2-fill"></i>
-      </li>
-      <i className="ri-menu-3-line"></i>
-    </ul>
-  </div>
-);
-
+import Navbar from "./components/navbar";
 const cardContent = [
   {
     image:
@@ -75,7 +52,7 @@ const Body = () => {
 const AppLayout = () => {
   return (
     <>
-      <Header />
+      <Navbar />
       <Body />
     </>
   );
---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => (
+  <div className="navbar">
+    <div className="logo">
+      FoodFunk
+      <i className="ri-bowl-fill"></i>
+    </div>
+    <ul>
+      <li>
+        Home<i className="ri-home-smile-fill"></i>
+      </li>
+      <li>
+        About<i className="ri-hotel-fill"></i>
+      </li>
+      <li>
+        Offers<i className="ri-money-rupee-circle-fill"></i>
+      </li>
+      <li>
+        Cart<i className="ri-shopping-cart-2-fill"></i>
+      </li>
+      <i className="ri-menu-3-line"></i>
+    </ul>
+  </div>
+);
+export default Navbar;
