9590da6c5304c241093083062f3064c141d441a6
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-18 16:17:33 +0000

    add footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 const AppLayout = () => {
   return (
     <>
       <Navbar />
       <Body />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright © 2024 </p>
+    </footer>
+  );
+};
+export default Footer;
