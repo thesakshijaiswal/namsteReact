c0468fb80b9cdc4d78d9868b33ef1dd54baac05d
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-18 16:31:16 +0000

    create searchbar and search button

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,11 +2,22 @@ import RestaurantCard from "./RestaurantCard";
 import { cardContent } from "../constants";
 const Body = () => {
   return (
-    <div className="restaurantCard">
-      {cardContent.map((restaurant) => {
-        return <RestaurantCard {...restaurant} key={restaurant.id} />;
-      })}
-    </div>
+    <>
+      <div className="searchBar">
+        <input
+          type="text"
+          className="searchBar"
+          id="searchBar"
+          placeholder="Search"
+        />
+        <button className="searchBtn">Search</button>
+      </div>
+      <div className="restaurantCard">
+        {cardContent.map((restaurant) => {
+          return <RestaurantCard {...restaurant} key={restaurant.id} />;
+        })}
+      </div>
+    </>
   );
 };
 export default Body;
