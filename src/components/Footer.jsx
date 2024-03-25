2a9d774aaf123b36b87ecf9cf4d791c3a1a30da9
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-03-25 17:08:30 +0000

    make useState counter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
 const Footer = () => {
+  const [value, setValue] = useState(0);
   return (
     <footer>
+      <div className="container">
+        <button
+          onClick={() => {
+            setValue(value - 1);
+          }}
+        >
+          -
+        </button>
+        <h3>{value}</h3>
+        <button
+          onClick={() => {
+            setValue(value + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
       <p>Copyright © 2024 </p>
     </footer>
   );
