1a65d7932948ea1269d1f235ca9bdd5d229e08e4
Sakshi Jaiswal <144778485+thesakshijaiswal@users.noreply.github.com>
2024-04-02 15:35:39 +0000

    add shimmer to restaurant card

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -2,9 +2,12 @@ import RestaurantCard from "./RestaurantCard";
 import "../../shimmer.css";
 const Shimmer = () => {
   return (
-    <>
-      <RestaurantCard />
-    </>
+    <div className="card">
+      <div className="cardImg"></div>
+      <h2></h2>
+      <p></p>
+      <h4></h4>
+    </div>
   );
 };
 export default Shimmer;
