b7f76f36be7026743c517e1182bcc62b8d48f17a
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-08-25 19:20:38 +0530

    style: remove vanilla stylesheet

--- src/shimmer/ShimmerHome.jsx
+++ src/shimmer/ShimmerHome.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ShimmerCard from "./ShimmerCard";
-import "../../style.css";
 const HomeShimmer = () => {
   const shimmerUICards = [];
   for (let i = 0; i < 20; i++) {
